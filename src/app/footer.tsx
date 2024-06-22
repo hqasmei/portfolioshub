944ddb7aa93315a79f9edbc6cc04d1c35e399bdb
Hosna Qasmei <hosna.qasmei@gmail.com>
2024-06-21 20:25:11 -0700

    Updated footer

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -2,19 +2,22 @@
 
 import Link from 'next/link';
 
+import { GithubIcon } from '@/components/icons/github';
 import { CONFIG } from '@/config';
 
 export function Footer() {
   return (
-    <footer className="items-center flex  justify-center gap-1 h-16">
-      Made with ❤️ by{' '}
-      <Link
-        href={CONFIG.twitter}
-        className="font-semibold hover:underline duration-200"
-        target="_blank"
-      >
-        Hosna Qasmei
-      </Link>
+    <footer className="items-center flex  justify-center gap-1 h-16 container">
+      <div className="flex w-full flex-col items-center justify-between gap-5 border-t border-gray-900/5 py-8 sm:flex-row dark:border-white/5">
+        <p className="text-xs text-muted-foreground ">
+          © {new Date().getFullYear()} PortfoliosHub. All rights reserved.
+        </p>
+        <div className="flex gap-4">
+          <Link target="_blank" href={CONFIG.github}>
+            <GithubIcon />
+          </Link>
+        </div>
+      </div>
     </footer>
   );
 }
