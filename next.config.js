a1e8ce549bddffacfed6c98b3a0583a9d29c7b08
Hosna Qasmei <hosna.qasmei@gmail.com>
2024-06-25 16:19:10 -0700

    Added more to next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,16 @@ const nextConfig = {
   images: { 
     remotePatterns: [ 
       {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        hostname: 'portfolioshub.com',
+      },
+      {
         hostname: 'tremendous-panda-612.convex.site',
       }, 
-   {
+      {
         hostname: 'spotted-swordfish-236.convex.site',
       }, 
     ],
