4d6e2662788b04f915f3ccccc17c862e477928ad
Hosna Qasmei <hosna.qasmei@gmail.com>
2024-06-29 09:56:36 -0700

    Updated middleware to see if it'll work

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,3 +1,5 @@
+import { NextResponse } from 'next/server';
+
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/']);
@@ -6,6 +8,8 @@ export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
     auth().protect();
   }
+
+  if (auth().userId && !isPublicRoute(request)) return NextResponse.next();
 });
 
 export const config = {
