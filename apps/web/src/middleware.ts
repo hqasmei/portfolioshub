73439418383299887288e38bb6a9adf928bf4552
Hosna Qasmei <hosna.qasmei@gmail.com>
2024-06-29 12:22:04 -0700

    Try again

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,9 +1,15 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher([
+  '/dashboard(.*)',
+  '/favorites(.*)',
+  '/admin(.*)',
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
 
 export const config = {
-  // The following matcher runs middleware on all routes
-  // except static assets.
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
