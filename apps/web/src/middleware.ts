db54a7ce91551756fd28410ef50faab42bfc8c5b
Hosna Qasmei <hosna.qasmei@gmail.com>
2024-06-29 02:14:37 -0700

    Updated middleware again

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,9 +1,13 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/']);
+
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect();
+  }
+});
 
 export const config = {
-  // The following matcher runs middleware on all routes
-  // except static assets.
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
