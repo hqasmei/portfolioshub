dc54d3b4ab56b19b039720027961e83249344a2a
Hosna Qasmei <hosna.qasmei@gmail.com>
2024-07-02 16:40:54 -0700

    Added back in Feedback

--- src/app/(main)/_components/header.tsx
+++ src/app/(main)/_components/header.tsx
@@ -22,14 +22,15 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import { api } from '@/convex/_generated/api';
 import useScroll from '@/hooks/use-scroll';
 import { useSession } from '@/lib/client-auth';
 import { cn } from '@/lib/utils';
 import { SignInButton, UserButton } from '@clerk/nextjs';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { api } from '@/convex/_generated/api';
 import { useMutation, useQuery } from 'convex/react';
-import { Loader2 } from 'lucide-react';
+import { Loader2, MessageSquareMore, Send } from 'lucide-react';
 import { useForm } from 'react-hook-form';
 import { toast } from 'sonner';
 import { z } from 'zod';
@@ -38,6 +39,101 @@ const submissionFormSchema = z.object({
   name: z.string().min(2).max(50),
   link: z.string().url(),
 });
+
+const feedbackFormSchema = z.object({
+  name: z.string().min(2).max(50),
+  email: z.string().email(),
+  feedback: z.string().min(2).max(500),
+});
+
+function FeedbackForm({ setOpen }: { setOpen: any }) {
+  const form = useForm<z.infer<typeof feedbackFormSchema>>({
+    resolver: zodResolver(feedbackFormSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      feedback: '',
+    },
+  });
+
+  const isSubmitting = form.formState.isSubmitting;
+  async function onSubmit(values: z.infer<typeof feedbackFormSchema>) {
+    await fetch('https://projectplannerai.com/api/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        projectId: process.env.NEXT_PUBLIC_PROJECT_PLANNER_AI_ID,
+        feedback: values.feedback,
+        name: values.name,
+        email: values.email,
+      }),
+    });
+    setOpen(false);
+    toast.success('Feedback successfully submitted! ');
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Your name</FormLabel>
+              <FormControl>
+                <Input placeholder="Hosna Qasmei" {...field} autoFocus />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Your email</FormLabel>
+              <FormControl>
+                <Input placeholder="test@example.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="feedback"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Feedback</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Can you please add light mode"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <div className="justify-end w-full flex">
+          <Button type="submit" disabled={isSubmitting}>
+            {isSubmitting ? (
+              <Loader2 className="animate-spin h-4 w-4" />
+            ) : (
+              <span>Submit</span>
+            )}
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+}
 
 function SubmissionForm({ setOpen }: { setOpen: any }) {
   const form = useForm<z.infer<typeof submissionFormSchema>>({
@@ -112,6 +208,8 @@ export function Header() {
   const session = useSession();
   const scrolled = useScroll(70);
   const [isSubimtOpen, setIsSubimtOpen] = useState(false);
+  const [isFeedbackOpen, setIsFeedbackOpen] = useState(false);
+
   const isAdmin = useQuery(api.users.isAdmin);
   const isHome = pathname === '/';
   return (
@@ -167,23 +265,49 @@ export function Header() {
               )}
             </div>
             <div className="flex flex-row items-center gap-2">
-              <Button
-                size="sm"
-                variant="link"
-                onClick={() => setIsSubimtOpen(true)}
-              >
-                Submit
-              </Button>
               {session.isLoggedIn ? (
-                <div className='hidden md:flex'>
+                <>
                   {isHome ? (
-                    <Button size="sm" asChild>
-                      <Link href="/dashboard">Dashboard</Link>
-                    </Button>
+                    <>
+                      <Button
+                        size="sm"
+                        variant="outline"
+                        onClick={() => setIsSubimtOpen(true)}
+                        className="flex gap-2"
+                      >
+                        <Send size={16} />
+                        <span className="hidden sm:inline">Submit</span>
+                      </Button>
+
+                      <Button size="sm" asChild className="hidden md:flex">
+                        <Link href="/dashboard">Dashboard</Link>
+                      </Button>
+                    </>
                   ) : (
-                    <UserButton />
+                    <>
+                      <Button
+                        variant="outline"
+                        size="sm"
+                        onClick={() => setIsFeedbackOpen(true)}
+                        className="flex gap-2"
+                      >
+                        <MessageSquareMore size={16} />
+                        <span className="hidden sm:inline">Feedback</span>
+                      </Button>
+                      <Button
+                        size="sm"
+                        onClick={() => setIsSubimtOpen(true)}
+                        className="flex gap-2"
+                      >
+                        <Send size={16} />
+                        <span className="hidden sm:inline">Submit</span>
+                      </Button>
+                      <div className="hidden md:flex md:ml-2">
+                        <UserButton />
+                      </div>
+                    </>
                   )}
-                </div>
+                </>
               ) : (
                 <Button size="sm" asChild>
                   <SignInButton mode="modal">Login</SignInButton>
@@ -193,6 +317,15 @@ export function Header() {
           </div>
         </nav>
       </header>
+
+      <Dialog open={isFeedbackOpen} onOpenChange={setIsFeedbackOpen}>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Feedback</DialogTitle>
+          </DialogHeader>
+          <FeedbackForm setOpen={setIsFeedbackOpen} />
+        </DialogContent>
+      </Dialog>
 
       <Dialog open={isSubimtOpen} onOpenChange={setIsSubimtOpen}>
         <DialogContent className="sm:max-w-[425px]">
