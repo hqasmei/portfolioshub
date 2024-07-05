80f00e8d0ac35bb8ec5818e345bdcc4b44e5a89c
Hosna Qasmei <hosna.qasmei@gmail.com>
2024-07-05 01:39:06 -0700

    Update banner again

--- src/app/(main)/_components/banner.tsx
+++ src/app/(main)/_components/banner.tsx
@@ -9,9 +9,9 @@ import { Button } from '@/components/ui/button';
 export default function Banner() {
   return (
     <div className="flex flex-col px-4 sm:flex-row gap-4 items-center justify-center w-full py-4  tracking-tight text-white font-medium bg-emerald-600 duration-200 transition-all  text-center group  text-lg">
-      🚀
+     
       <span className='font-light'>
-        <span className="font-bold">Simple Project Management</span> for side
+        <span className="font-bold">✨ Simple Project Management</span> for side
         projects and freelancers
       </span>
       <Button size="sm" asChild className="w-full sm:w-fit">
