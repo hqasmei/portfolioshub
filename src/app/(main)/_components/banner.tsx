348064f37b167ff906c7df1e9114ac94749cc47e
Hosna Qasmei <hosna.qasmei@gmail.com>
2024-07-05 01:37:43 -0700

    Update banner

--- src/app/(main)/_components/banner.tsx
+++ src/app/(main)/_components/banner.tsx
@@ -1,22 +1,24 @@
+'use client';
+
 import React from 'react';
 
 import Link from 'next/link';
 
-import { ChevronRight } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 
 export default function Banner() {
   return (
-    <Link
-      target="_blank"
-      href="https://projectplannerai.com/"
-      className="flex flex-row items-center justify-center w-full h-12 tracking-tight text-white font-medium bg-emerald-600 hover:bg-emerald-500 duration-200 transition-all  text-center group text-balance text-sm sm:text-base"
-    >
-      🚀 Introducing ProjectPlannerAI - Simplify your project management and
-      finally finish your projects{' '}
-      <ChevronRight
-        size={18}
-        className="hidden md:block ml-1 group-hover:translate-x-1 transition-all duration-300"
-      />
-    </Link>
+    <div className="flex flex-col px-4 sm:flex-row gap-4 items-center justify-center w-full py-4  tracking-tight text-white font-medium bg-emerald-600 duration-200 transition-all  text-center group  text-lg">
+      🚀
+      <span className='font-light'>
+        <span className="font-bold">Simple Project Management</span> for side
+        projects and freelancers
+      </span>
+      <Button size="sm" asChild className="w-full sm:w-fit">
+        <Link href="https://projectplannerai.com/" target="_blank">
+          Try Now
+        </Link>
+      </Button>
+    </div>
   );
 }
