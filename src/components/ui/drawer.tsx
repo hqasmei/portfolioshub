722548a6e885fa1511976a2aa03525480b7e8140
Hosna Qasmei <hosna.qasmei@gmail.com>
2024-07-07 16:37:29 -0700

    Updated drawer rounded

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -43,7 +43,7 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        'fixed inset-x-0 bottom-0 z-50 flex h-auto flex-col rounded-b-[10px] border bg-background',
+        'fixed inset-x-0 bottom-0 z-50 flex h-auto flex-col rounded-t-[10px] border bg-background',
         className,
       )}
       {...props}
