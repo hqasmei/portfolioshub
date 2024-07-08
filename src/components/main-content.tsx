7e955ef346ba9a245bae6e9558283d8cf6389a3b
Hosna Qasmei <hosna.qasmei@gmail.com>
2024-07-07 19:33:03 -0700

    Implemented infinite scrolling in front end

--- src/components/main-content.tsx
+++ src/components/main-content.tsx
@@ -1,18 +1,14 @@
-'use client';
+'use strict';
 
 import React, { useEffect, useState } from 'react';
 
-
-
 import { api } from '@/convex/_generated/api';
 import { useQuery } from 'convex/react';
-
-
+import { useInView } from 'react-intersection-observer';
 
 import MainContentSkeleton from './main-content-skeleton';
 import PortfolioCard from './portfolio-card';
 import { Button } from './ui/button';
-
 
 export default function MainContent({
   selectedSort,
@@ -24,27 +20,17 @@ export default function MainContent({
   searchValue: string;
 }) {
   const [favorites, setFavorites] = useState<Map<string, string>>(new Map());
-
   const [visibleCount, setVisibleCount] = useState(6);
+  const [loading, setLoading] = useState(false);
+  const [hasMoreData, setHasMoreData] = useState(true);
+  const [scrollTrigger, isInView] = useInView();
+
   const portfolios = useQuery(api.portfolios.getPortfolios, {
     sortType: selectedSort || 'recentlyAdded',
   });
 
   const isLoading = portfolios === undefined || portfolios.length === 0;
-
   const getFavoritesForUser = useQuery(api.favorites.getFavoritesForUser);
-
-  const handleLoadMore = () => {
-    setVisibleCount((prevCount) => prevCount + 6);
-  };
-  useEffect(() => {
-    if (getFavoritesForUser) {
-      const favoriteMap = new Map(
-        getFavoritesForUser.map((fav) => [fav.portfolioId, fav._id]),
-      );
-      setFavorites(favoriteMap);
-    }
-  }, [getFavoritesForUser]);
 
   const filteredData =
     selectedFilter === 'All' || selectedFilter === null || !portfolios
@@ -61,7 +47,32 @@ export default function MainContent({
           .filter((portfolio) =>
             portfolio.name.toLowerCase().includes(searchValue.toLowerCase()),
           );
- 
+
+  const handleLoadMore = () => {
+    setLoading(true);
+    setVisibleCount((prevCount) => prevCount + 6);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (getFavoritesForUser) {
+      const favoriteMap = new Map(
+        getFavoritesForUser.map((fav) => [fav.portfolioId, fav._id]),
+      );
+      setFavorites(favoriteMap);
+    }
+  }, [getFavoritesForUser]);
+
+  useEffect(() => {
+    if (isInView && hasMoreData) {
+      handleLoadMore();
+    }
+  }, [isInView, hasMoreData]);
+
+  useEffect(() => {
+    setHasMoreData(visibleCount < (filteredData?.length || 0));
+  }, [visibleCount, filteredData]);
+
   if (isLoading) {
     return <MainContentSkeleton />;
   }
@@ -69,14 +80,21 @@ export default function MainContent({
   return (
     <div className="flex flex-col gap-2 pb-4 md:pb-4">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-6">
-        {filteredData?.slice(0, visibleCount).map((item, idx) => {
-          return <PortfolioCard key={idx} item={item} favorites={favorites} />;
-        })}
+        {filteredData
+          ?.slice(0, visibleCount)
+          .map((item, idx) => (
+            <PortfolioCard key={idx} item={item} favorites={favorites} />
+          ))}
       </div>
-
-      {filteredData && visibleCount < filteredData.length && (
+      {filteredData && hasMoreData && (
         <div className="flex justify-center pt-4">
-          <Button onClick={handleLoadMore}>Load More</Button>
+          <Button
+            ref={scrollTrigger}
+            onClick={handleLoadMore}
+            disabled={loading}
+          >
+            {loading ? 'Loading...' : 'Load More'}
+          </Button>
         </div>
       )}
     </div>
