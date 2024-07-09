d9ee742d2d9390e31f10b16b3dd812c374c8ba9a
Hosna Qasmei <hosna.qasmei@gmail.com>
2024-07-09 12:54:47 -0700

    Made the search bar better

--- src/components/search-box.tsx
+++ src/components/search-box.tsx
@@ -32,17 +32,17 @@ export default function SearchBox({
           value={searchValue}
           onChange={(e) => setSearchValue(e.target.value)}
           placeholder="Search"
-          className={`absolute inset-0 w-full h-full rounded-full  outline-none pl-5 pr-14 ${active ? 'opacity-100' : 'opacity-0'} transition-opacity duration-500`}
+          className={`absolute inset-0 w-full h-full rounded-full  outline-none px-9 ${active ? 'opacity-100' : 'opacity-0'} transition-opacity duration-500`}
         />
         <div
-          className={`absolute right-0 top-0 bottom-0 flex items-center justify-center w-9 h-9 rounded-full cursor-pointer transition-all duration-500 ${active ? 'transform translate-x-0' : ''}`}
+          className={`flex items-center justify-center w-9 h-9 rounded-full cursor-pointer transition-all duration-500 ${active ? 'transform translate-x-0' : 'absolute right-0 top-0 bottom-0'}`}
           onClick={handleSearchClick}
         >
           <Search size={18} className="mb-0.5 ml-0.5" />
         </div>
         {active && searchValue !== '' && (
           <div
-            className="absolute right-8 top-0 bottom-0 flex items-center cursor-pointer transition-all duration-500"
+            className="absolute right-2 top-0 bottom-0 flex items-center cursor-pointer transition-all duration-500"
             onClick={handleCancelClick}
           >
             <X size={22} className="stroke-muted-foreground" />
